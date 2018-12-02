6de55765165f4bb80a991dddf7b4e5b68ee9d2b8
buweiling <bwlloveistrue@163.com>
2018-12-02 11:07:06 +0800

    首次提交框架

---
+++ src/main/java/com/logisticscenter/logisticscenter/LogisticscenterApplication.java
@@ -0,0 +1,12 @@
+package com.logisticscenter.logisticscenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticscenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticscenterApplication.class, args);
+	}
+}
